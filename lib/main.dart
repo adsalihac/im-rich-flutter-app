bc6ae06e5644fb4c4f3f8da1afaa3db303be0372
adsalihac <adsalihac@gmail.com>
2022-03-31 19:06:03 +0530

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ void main() {
         body: Center(
           child: Center(
             child: Image(
-              image: NetworkImage(
-                'https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2FtcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
-              ),
+              image: AssetImage('images/diamond.png'),
             ),
           ),
         ),
